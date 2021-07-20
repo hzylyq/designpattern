67607c27433072812d43f302c367ff6c214b0068
huzhiyang <164177565@qq.com>
2021-07-20 13:42:42 +0800

    chore: chain

--- chainofresponsibility/support.go
+++ chainofresponsibility/support.go
@@ -1,4 +1,20 @@
 package chainofresponsibility
 
-type support interface {
+type Re
+
+type support struct {
+
+	Next *support
+}
+
+func (s *support) SetNext(next *support) {
+	s.Next = next
+}
+
+func (s *support) Support(t trouble) {
+
+
+	if s.Next != nil {
+		s.Next.Support(t)
+	}
 }
