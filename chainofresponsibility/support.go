3e1dea4686ec8d1bd964037177425ed528d4f579
huzhiyang <164177565@qq.com>
2021-07-21 13:54:20 +0800

    chore: chain

--- chainofresponsibility/support.go
+++ chainofresponsibility/support.go
@@ -1,20 +1,35 @@
 package chainofresponsibility
 
-type Re
+import "log"
+
+type Manager interface {
+	resolve(t trouble) bool
+}
 
 type support struct {
-
+	Manager
 	Next *support
+	Name string
 }
 
 func (s *support) SetNext(next *support) {
 	s.Next = next
 }
 
+func (s *support) Done(t trouble) {
+	log.Println(t.ToString() + " is resolved by " + s.Name)
+}
+
+func (s *support) Fail(t trouble) {
+	log.Println(t.ToString() + " cannot be resolved.")
+}
+
 func (s *support) Support(t trouble) {
-
-
-	if s.Next != nil {
+	if s.Manager.resolve(t) {
+		s.Done(t)
+	} else if s.Next != nil {
 		s.Next.Support(t)
+	} else {
+		s.Fail(t)
 	}
 }
